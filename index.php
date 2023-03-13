ebac6418a143a1791d014e3a15aae7df1007abce
Strxct <calvinhofman3@gmail.com>
2023-03-13 10:04:21 +0100

    build the homepage in desktop working on mobile

--- index.php
+++ index.php
@@ -6,26 +6,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="styles/style.css">
 
     <title>Hello, world!</title>
 </head>
-<body>
+<body class="">
 <?php require_once('components/header.php'); ?>
-<header>
-    <div class="header__bg"></div>
-    <h1>Header Content</h1>
-    test
-</header>
+<svg class="absolute z-10" width="1443" height="951" viewBox="0 0 1443 951" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1440.59 475.597C1475.63 888.507 1078.69 867.195 0.871216 950.28V0.898692C523.745 128.959 1405.54 62.6862 1440.59 475.597Z" fill="#49A078"/>
+</svg>
+<main class="absolute z-20 w-full">
+    <div>
+        <img class="mx-auto mt-40 " src="https://picsum.photos/seed/picsum/1200/300" alt="" srcset="">
+    </div>
+    <div class="flex flex-row flex-wrap justify-between items-center mt-40 w-8/12 mx-auto border-b-4 border-black pb-16">
+        <div class="w-96">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus dolorum minus nostrum obcaecati voluptate! Eveniet accusantium temporibus tempore animi libero soluta maxime doloremque ullam, ratione magnam beatae distinctio vitae aliquid labore ipsa autem, rerum debitis illum consequatur nesciunt quos. Officiis.</p>
+        </div>
+        <div class="">
+            <img class="mx-auto " src="https://picsum.photos/seed/picsum/600/300" alt="" srcset="">
+        </div>
+    </div>
+    <div class="mt-40">
+        <img class="mx-auto " src="https://picsum.photos/seed/picsum/1920/300" alt="" srcset="">
+    </div>
+    <div class="flex flex-row flex-wrap justify-between items-center mt-40 w-8/12 mx-auto border-b-4 border-black pb-16">
+        <div class="">
+            <img class="mx-auto " src="https://picsum.photos/seed/picsum/600/300" alt="" srcset="">
+        </div>
+        <div class="w-96">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus dolorum minus nostrum obcaecati voluptate! Eveniet accusantium temporibus tempore animi libero soluta maxime doloremque ullam, ratione magnam beatae distinctio vitae aliquid labore ipsa autem, rerum debitis illum consequatur nesciunt quos. Officiis.</p>
+        </div>
+    </div>
+    <div class="flex flex-row flex-wrap justify-between w-8/12 mx-auto mt-40">
+        <div class="flex flex-col">
+            <img class="mx-auto rounded-full" src="https://picsum.photos/seed/picsum/200/200" alt="" srcset="">
+            <p class="w-64 text-center mt-12">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia fugiat et, doloribus voluptatum iure asperiores earum nemo totam quo! Praesentium impedit saepe possimus illum, quas sint nostrum inventore nulla sed?</p>
+        </div>
+        <div class="flex flex-col">
+            <img class="mx-auto rounded-full" src="https://picsum.photos/seed/picsum/200/200" alt="" srcset="">
+            <p class="w-64 text-center mt-12">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia fugiat et, doloribus voluptatum iure asperiores earum nemo totam quo! Praesentium impedit saepe possimus illum, quas sint nostrum inventore nulla sed?</p>
+        </div>
+        <div class="flex flex-col">
+            <img class="mx-auto rounded-full" src="https://picsum.photos/seed/picsum/200/200" alt="" srcset="">
+            <p class="w-64 text-center mt-12">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia fugiat et, doloribus voluptatum iure asperiores earum nemo totam quo! Praesentium impedit saepe possimus illum, quas sint nostrum inventore nulla sed?</p>
+        </div>
+        <div class="flex flex-col">
+            <img class="mx-auto rounded-full" src="https://picsum.photos/seed/picsum/200/200" alt="" srcset="">
+            <p class="w-64 text-center mt-12">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia fugiat et, doloribus voluptatum iure asperiores earum nemo totam quo! Praesentium impedit saepe possimus illum, quas sint nostrum inventore nulla sed?</p>
+        </div>
+    </div>
+</main>
+<footer>
 
+</footer>
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
