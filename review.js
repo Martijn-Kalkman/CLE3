7dcf07ab6dbc13c3ba619649b85a4378028ec48a
Flurry19 <sharonirenedewit@gmail.com>
2023-03-28 10:02:38 +0200

    sharon's webservice

--- review.js
+++ review.js
@@ -1,12 +1,56 @@
-window.addEventListener('', init)
-let user;
-let stars;
-let review;
+window.addEventListener('load', init)
+let apiUrl = 'http://localhost/School/Jaar-1/3e_kwartaal/CLE3/webservice-sharon/index.php';
+let allReviews = document.getElementById('allreviews');
 
-function init{
-user = document.getElementById('user').value;
-    review = document.getElementById('review').value;
-    if(user !== '' && review !== ''){
-    let li = document.getElementById('li');
+function init(){
+    seeReviews();
 }
+
+function seeReviews(){
+    fetch(apiUrl)
+        .then((response) => {
+            if(!response.ok){
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(displayReviews)
+        .catch(ajaxErrorHandler);
+}
+
+function displayReviews(data){
+    console.log(data);
+    console.log(data.id);
+    console.log(data.name);
+    for (let review of data.results) {
+
+        let div = document.createElement('div');
+        div.classList.add('reviewcard');
+        div.dataset.name = review.name
+        allReviews.appendChild(div);
+
+    let name = document.createElement('li');
+    name.classList.add('reviewname');
+    name.innerText = review.name;
+    div.appendChild(name);
+
+    let stars = document.createElement('li');
+    stars.classList.add('rating');
+    stars.innerText = review.rating;
+    div.appendChild(stars);
+
+
+    let description = document.createElement('li');
+    description.classList.add('reviewdescription');
+    description.innerText = review.feedback;
+    div.appendChild(description);
+}
+}
+
+function ajaxErrorHandler(data){
+    console.log(data);
+    let error = document.createElement('div');
+    error.classList.add('error');
+    error.innerText = "Op het moment kunnen de reviews niet geladen worden, probeer het later opnieuw"
+    allReviews.before(error);
 }
--- webservice-sharon/includes/actions.php
+++ webservice-sharon/includes/actions.php
@@ -12,27 +12,27 @@ function getReviews()
         [
             "id" => 1,
             "name" => "Bob",
-            "stars" => "2",
+            "rating" => "2",
         ],
         [
             "id" => 2,
             "name" => "Hannah",
-            "stars" => "1",
+            "rating" => "1",
         ],
         [
             "id" => 3,
             "name" => "Bert",
-            "stars" => "3",
+            "rating" => "3",
         ],
         [
             "id" => 4,
             "name" => "Sandra",
-            "stars" => "2",
+            "rating" => "2",
         ],
         [
             "id" => 5,
             "name" => "Belle",
-            "stars" => "1",
+            "rating" => "1",
         ]
     ];
 }
@@ -45,23 +45,23 @@ function getReviewDetails($id)
 {
     $tags = [
         1 => [
-            "feedback" => "De lift werkt 9/10 keren niet, ik raad zeker niet aan om gebruik te maken van dit station.",
+            "comment" => "De lift werkt 9/10 keren niet, ik raad zeker niet aan om gebruik te maken van dit station.",
             "tags" => ['lift', 'afgeraden']
         ],
         2 => [
-            "feedback" => "De lift (als hij al werkt) stinkt enorm naar urine.",
+            "comment" => "De lift (als hij al werkt) stinkt enorm naar urine.",
             "tags" => ['lift', 'afgeraden']
         ],
         3 => [
-            "feedback" => "Vaak werkt de lift niet maar gelukkig zijn er twee.",
+            "comment" => "Vaak werkt de lift niet maar gelukkig zijn er twee.",
             "tags" => ['lift'],
         ],
         4 => [
-            "feedback" => "Er lopen weinig werknemers rond om te helpen mocht het nodig zijn.",
+            "comment" => "Er lopen weinig werknemers rond om te helpen mocht het nodig zijn.",
             "tags" => ['werknemers', 'afgeraden']
         ],
         5 => [
-            "feedback" => "Er zit een stoeptegel los waardoor ik het station niet op kom.",
+            "comment" => "Er zit een stoeptegel los waardoor ik het station niet op kom.",
             "tags" => ['stoeptegel', 'afgeraden']
         ],
     ];
