1e427fdb3ac450d5813ea947aed507ecc73f5d61
Flurry19 <sharonirenedewit@gmail.com>
2023-03-31 20:30:41 +0200

    sharon's webservice

--- review.js
+++ review.js
@@ -3,12 +3,21 @@ window.addEventListener('load', init);
 // Globals
 let apiUrl = 'http://localhost/School/Jaar-1/3e_kwartaal/CLE3/webservice-sharon/index.php';
 let allReviews;
+let reviewData = {};
+let reviewDetails;
+let detailContent;
 
 // Start function
 function init()
 {
     allReviews = document.getElementById('all-reviews');
     ajaxRequest(apiUrl, createReviewCards);
+    allReviews.addEventListener('click', clickedReview);
+
+    reviewDetails = document.getElementById('review-detail');
+    detailContent = document.querySelectorAll('.modal-content');
+    reviewDetails.addEventListener('click', detailModalClickHandler);
+    reviewDetails.addEventListener('close', detailCloseHandler);
 }
 
 //Function for fetching the API
@@ -28,50 +37,150 @@ function ajaxRequest(url, succesHandler){
 
 // Function create a card with a review of a person
 function createReviewCards(data) {
-    // console.log(data);
     for (let review of data.results) {
         console.log(data);
         let div = document.createElement('div');
         div.classList.add('review-card');
         div.dataset.name = review.name;
 
-        let name = document.createElement('h3');
-        name.classList.add('reviewname');
+
+        let name = document.createElement('h2');
+        name.classList.add('text-3xl', 'font-bold', 'dark:text-black');
         name.innerText = review.name;
         div.appendChild(name);
 
-        let rating = document.createElement('div');
-        rating.classList.add('rating');
-        rating.innerText = review.rating;
-        div.appendChild(rating);
+
+        if (review.rating === 1){
+          let stars = document.createElement('span');
+           stars.classList.add('fa', 'fa-star', 'checked', 'allstars');
+            div.appendChild(stars);
+        }
+
+        if (review.rating === 2){
+            let stars = document.createElement('span');
+            stars.classList.add('fa', 'fa-star', 'checked', 'allstars');
+            div.appendChild(stars.cloneNode(true));
+            div.appendChild(stars.cloneNode(true));
+        }
+
+        if (review.rating === 3){
+            let stars = document.createElement('span');
+            stars.classList.add('fa', 'fa-star', 'checked', 'allstars');
+            div.appendChild(stars.cloneNode(true));
+            div.appendChild(stars.cloneNode(true));
+            div.appendChild(stars.cloneNode(true));
+        }
+        if (review.rating === 4){
+            let stars = document.createElement('span');
+            stars.classList.add('fa', 'fa-star', 'checked', 'allstars');
+            div.appendChild(stars.cloneNode(true));
+            div.appendChild(stars.cloneNode(true));
+            div.appendChild(stars.cloneNode(true));
+            div.appendChild(stars.cloneNode(true));
+        }
+        if (review.rating === 5){
+            let stars = document.createElement('span');
+            stars.classList.add('fa', 'fa-star', 'checked', 'allstars');
+            div.appendChild(stars.cloneNode(true));
+            div.appendChild(stars.cloneNode(true));
+            div.appendChild(stars.cloneNode(true));
+            div.appendChild(stars.cloneNode(true));
+            div.appendChild(stars.cloneNode(true));
+        }
+
+
         allReviews.appendChild(div);
 
-        ajaxRequest(apiUrl, fillReviews);
+        let button = document.createElement('button');
+        button.classList.add('bg-blue-500', 'hover:bg-blue-400', 'text-white', 'font-bold', 'py-2', 'px-4', 'border-b-4', 'border-blue-700', 'hover:border-blue-500', 'rounded');
+        button.innerText = 'Bekijk review';
+        div.appendChild(button);
+        ajaxRequest(apiUrl, average);
     }
 
 }
 
-//Function write the name and rating from the array
-function fillReviews(review){
-    let div = document.querySelector(`.review-card[data-name='${review.name}']`);
+//Function write the name and rating from the array in a separate screen
+function clickedReview(e) {
+    let clickedItem = e.target;
+    if (clickedItem.nodeName !== 'BUTTON'){
+        return;
+    }
+
+    let review = reviewData[clickedItem.dataset.id];
+    console.log(review);
+
+    detailContent.innerHTML = '';
+
+    // let name = document.createElement('h2');
+    // name.classList.add('text-4xl', 'font-extrabold', 'dark:text-black');
+    // name.innerHTML = `${review.name} (#${review.id})`;
+    // reviewDetails.appendChild(name);
 
 
-
-
+    // if (review.rating === 1){
+    //     let stars = document.createElement('span');
+    //     stars.classList.add('fa', 'fa-star', 'checked');
+    //     div.appendChild(stars);
+    // }
     //
-    // let button = document.createElement('button');
-    // button.classList.add('seemore');
-    // button.innerText = 'See more';
-    // div.appendChild(button);
+    // if (review.rating === 2){
+    //     let stars = document.createElement('span');
+    //     stars.classList.add('fa', 'fa-star', 'checked');
+    //     div.appendChild(stars.cloneNode(true));
+    //     div.appendChild(stars.cloneNode(true));
+    // }
+    //
+    // if (review.rating === 3){
+    //     let stars = document.createElement('span');
+    //     stars.classList.add('fa', 'fa-star', 'checked');
+    //     div.appendChild(stars.cloneNode(true));
+    //     div.appendChild(stars.cloneNode(true));
+    //     div.appendChild(stars.cloneNode(true));
+    // }
+    // if (review.rating === 4){
+    //     let stars = document.createElement('span');
+    //     stars.classList.add('fa', 'fa-star', 'checked');
+    //     div.appendChild(stars.cloneNode(true));
+    //     div.appendChild(stars.cloneNode(true));
+    //     div.appendChild(stars.cloneNode(true));
+    //     div.appendChild(stars.cloneNode(true));
+    // }
+    // if (review.rating === 5){
+    //     let stars = document.createElement('span');
+    //     stars.classList.add('fa', 'fa-star', 'checked');
+    //     div.appendChild(stars.cloneNode(true));
+    //     div.appendChild(stars.cloneNode(true));
+    //     div.appendChild(stars.cloneNode(true));
+    //     div.appendChild(stars.cloneNode(true));
+    //     div.appendChild(stars.cloneNode(true));
+    // }
 
-    // let description = document.createElement('li');
-    // description.classList.add('reviewdescription');
-    // description.innerText = review.feedback;
-    // div.appendChild(description);
-    // console.log(data);
+
+    reviewDetails.showModal();
+    allReviews.classList.add('dialog-open');
+
+    // reviewDetails.innerText = "Hoi"
 }
 
+function average(data){
+    let numbers = data.rating;
+    let sum = numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    })
+    let avg = sum/numbers.length;
+    console.log(avg);
+}
 
+function detailModalClickHandler(e){
+    if (e.target.nodeName === 'DIALOG' || e.target.nodeName === 'BUTTON') {
+        reviewDetails.close();
+    }
+}
+
+function detailCloseHandler() {
+    allReviews.classList.remove('dialog-open');
+}
 //Error function
 function ajaxErrorHandler(data){
     console.log(data);
--- review.php
+++ review.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="en">
 
@@ -5,22 +6,17 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- Bootstrap CSS -->
   <script src="https://cdn.tailwindcss.com"></script>
     <script src="review.js"></script>
 
 
-  <!-- Custom CSS -->
-  <link rel="stylesheet" href="./styles/style.css">
-  <link
-          rel="stylesheet"
-          href="https://unpkg.com/swiper/swiper-bundle.min.css"
-  />
-  <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-  />
+
   <title>Reviews-Handitransport</title>
 </head>
 <?php require_once('components/header.php'); ?>
@@ -43,70 +39,12 @@
     <div id="all-reviews">
 
     </div>
-<!--                                             review hoe ik het eruit wil laten zien-->
-<!--  <article>-->
-<!--    <div class="flex items-center mb-4 space-x-4">-->
-<!--      <div class="space-y-1 font-medium dark:text-white">-->
-<!--        <p>Jese Leos <time datetime="2014-08-16 19:00" class="block text-sm text-gray-500 dark:text-gray-400">16 August 2014</time></p>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--    <div class="flex items-center mb-1">-->
-<!--      <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>-->
-<!--      <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>-->
-<!--      <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>-->
-<!--      <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>-->
-<!--      <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>-->
-<!--    </div>-->
-<!--    <p class="mb-2 font-light text-gray-500 dark:text-gray-400">This is my third Invicta Pro Diver. They are just fantastic value for money. This one arrived yesterday and the first thing I did was set the time, popped on an identical strap from another Invicta and went in the shower with it to test the waterproofing.... No problems.</p>-->
-<!--    <p class="mb-3 font-light text-gray-500 dark:text-gray-400">It is obviously not the same build quality as those very expensive watches. But that is like comparing a Citroën to a Ferrari. This watch was well under £100! An absolute bargain.</p>-->
-<!--  </article>-->
-<!---->
-<!--      Gemiddelde achtergelaten reviews-->
-<!--  <div class="flex items-center mb-3">-->
-<!--    <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>-->
-<!--    <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>-->
-<!--    <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>-->
-<!--    <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>-->
-<!--    <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>-->
-<!--    <p class="ml-2 text-sm font-medium text-gray-900 dark:text-white">4.95 out of 5</p>-->
-<!--  </div>-->
-<!--  <p class="text-sm font-medium text-gray-500 dark:text-gray-400">... recensies</p>-->
-<!--  <div class="flex items-center mt-4">-->
-<!--    <span class="text-sm font-medium text-blue-600 dark:text-blue-500">5 ster</span>-->
-<!--    <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">-->
-<!--      <div class="h-5 bg-yellow-400 rounded" style="width: 70%"></div>-->
-<!--    </div>-->
-<!--    <span class="text-sm font-medium text-blue-600 dark:text-blue-500">70%</span>-->
-<!--  </div>-->
-<!--  <div class="flex items-center mt-4">-->
-<!--    <span class="text-sm font-medium text-blue-600 dark:text-blue-500">4 ster</span>-->
-<!--    <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">-->
-<!--      <div class="h-5 bg-yellow-400 rounded" style="width: 17%"></div>-->
-<!--    </div>-->
-<!--    <span class="text-sm font-medium text-blue-600 dark:text-blue-500">17%</span>-->
-<!--  </div>-->
-<!--  <div class="flex items-center mt-4">-->
-<!--    <span class="text-sm font-medium text-blue-600 dark:text-blue-500">3 ster</span>-->
-<!--    <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">-->
-<!--      <div class="h-5 bg-yellow-400 rounded" style="width: 8%"></div>-->
-<!--    </div>-->
-<!--    <span class="text-sm font-medium text-blue-600 dark:text-blue-500">8%</span>-->
-<!--  </div>-->
-<!--  <div class="flex items-center mt-4">-->
-<!--    <span class="text-sm font-medium text-blue-600 dark:text-blue-500">2 ster</span>-->
-<!--    <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">-->
-<!--      <div class="h-5 bg-yellow-400 rounded" style="width: 4%"></div>-->
-<!--    </div>-->
-<!--    <span class="text-sm font-medium text-blue-600 dark:text-blue-500">4%</span>-->
-<!--  </div>-->
-<!--  <div class="flex items-center mt-4">-->
-<!--    <span class="text-sm font-medium text-blue-600 dark:text-blue-500">1 ster</span>-->
-<!--    <div class="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">-->
-<!--      <div class="h-5 bg-yellow-400 rounded" style="width: 1%"></div>-->
-<!--    </div>-->
-<!--    <span class="text-sm font-medium text-blue-600 dark:text-blue-500">1%</span>-->
-<!--  </div>-->
-<!--  </div>-->
+<!--    dialoogvenster-->
+    <dialog class="modal" id="review-detail">
+        <div class="modal-content"></div>
+        <button class="modal-close">❌</button>
+    </dialog>
+
 
 </main>
 
